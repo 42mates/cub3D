940deab3da7ea18c8e328db676cd23491eaf21e7
Aleksei KUROCHKIN <akurochk@paul-f4Ar4s4.clusters.42paris.fr>
2024-09-10 17:04:49 +0200

    fix: glithch line at the left side of screen

--- src/ray_casting.c
+++ src/ray_casting.c
@@ -6,7 +6,7 @@
 /*   By: akurochk <akurochk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 17:15:04 by akurochk          #+#    #+#             */
-/*   Updated: 2024/09/09 17:10:55 by akurochk         ###   ########.fr       */
+/*   Updated: 2024/09/10 17:01:14 by akurochk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,7 +128,7 @@ void	ray_casting(t_data *data)
 
 	while (++n_line < SIZE_X) // to draw lines in the window
 	{
-		draw_line(SIZE_X - n_line, ray_scan(l_dir, data) * cos(data->dir_v - l_dir), data);
+		draw_line(SIZE_X - n_line - 1, ray_scan(l_dir, data) * cos(data->dir_v - l_dir), data);
 		l_dir += step;	// change direction to the next line
 	}
 }
